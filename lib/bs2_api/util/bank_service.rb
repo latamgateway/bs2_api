26873944cc299f9ddf4052cbb5735d923028260f
Kim Pastro <kimpastro@gmail.com>
2021-12-22 15:25:50 -0300

    parse to_s when find bank by code

--- lib/bs2_api/util/bank_service.rb
+++ lib/bs2_api/util/bank_service.rb
@@ -7,7 +7,7 @@ module Bs2Api
     class BankService
       class << self
         def find_by_code code
-          bank = bank_list.find {|b| b["code"] == code }
+          bank = bank_list.find {|b| b["code"].to_s == code.to_s }
           raise Bs2Api::Errors::MissingBank, "Bank with code #{code} not registered into util/banks.yml file" if bank.blank?
           bank
         end
