c1c83ddba548e18af7f73ca0e9930d42c65e0ed5
Kim Pastro <kimpastro@gmail.com>
2022-07-02 16:17:37 -0300

    proxy to detail

--- lib/bs2_api/payment/detail.rb
+++ lib/bs2_api/payment/detail.rb
@@ -29,7 +29,14 @@ module Bs2Api
       end
 
       def detail_request
-        HTTParty.get(url, headers: headers)
+        HTTParty.get(
+          url, 
+          headers: headers,
+          http_proxyaddr: @proxy&.host,
+          http_proxyport: @proxy&.port,
+          http_proxyuser: @proxy&.user,
+          http_proxypass: @proxy&.password
+        )
       end
 
       def bearer_token
